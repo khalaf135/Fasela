1f91e6e799c158fe36ed500e1fa4f6cce81ba561
khalaf135 <163313872+khalaf135@users.noreply.github.com>
2024-11-21 16:23:11 +0300

    Delete Fasela directory

--- Fasela/myplant.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const locatePlantBtn = document.getElementById("locatePlantBtn");
-
-  locatePlantBtn.addEventListener("click", () => {
-    alert("This feature will allow you to add a new plant based on your location. Still working in it");
-    // Add logic to open a map or navigate to the location page in the future.
-  });
-});
--- Fasela/profile.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const settingsBtn = document.querySelector(".settings-btn");
-  
-    settingsBtn.addEventListener("click", () => {
-      alert("Settings feature coming soon!");
-    });
-  });
